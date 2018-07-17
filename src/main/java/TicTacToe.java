0b96ccd36c8d47ecf5050cb78225b5e9ad3712f5
Matej <dobrakmato@gmail.com>
2018-07-17 14:19:00 +0200

    implement tests and basic game logic

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,90 @@
+public class TicTacToe {
+
+    public static final int PLAYER_X = 1;
+    public static final int PLAYER_O = -1;
+
+    private final int[] board = new int[9];
+    private int nextPlayer = 0;
+
+    public boolean isMoveLegal(int position) {
+        if (!isPositionLegal(position)) {
+            throw new IllegalArgumentException("Illegal position!");
+        }
+
+        return board[position] == 0;
+    }
+
+    private boolean isPositionLegal(int position) {
+        return position < 9;
+    }
+
+    public int getState(int position) {
+        if (!isPositionLegal(position)) {
+            throw new IllegalArgumentException("Illegal position!");
+        }
+
+        return board[position];
+    }
+
+    public void playMove(int player, int position) {
+        if (!isPositionLegal(position)) {
+            throw new IllegalArgumentException("Illegal position!");
+        }
+
+        if (!isMoveLegal(position)) {
+            throw new IllegalArgumentException("Illegal move played!");
+        }
+
+        if (player != nextPlayer && nextPlayer != 0) {
+            throw new IllegalArgumentException("Next player is " + nextPlayer);
+        }
+
+        board[position] = player;
+        if (nextPlayer == 0) {
+            nextPlayer = player * -1;
+        } else {
+            nextPlayer *= -1;
+        }
+    }
+
+    public boolean hasWon(int player) {
+        // horizontal check
+        for (int r = 0; r < 2; r++) {
+            if (board[3 * r] == player &&
+                    board[3 * r + 1] == player &&
+                    board[3 * r + 2] == player) {
+                return true;
+            }
+        }
+
+        // vertical check
+        for (int c = 0; c < 2; c++) {
+            if (board[c] == player &&
+                    board[c + 3] == player &&
+                    board[c + 6] == player) {
+                return true;
+            }
+        }
+
+        // diagonal check
+        if ((board[0] == player && board[4] == player && board[8] == player) ||
+                (board[2] == player && board[4] == player && board[6] == player)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isDraw() {
+        int movesLeft = 9;
+
+        for (int i = 0; i < 9; i++) {
+            if (!isMoveLegal(i)) {
+                movesLeft--;
+            }
+        }
+        return movesLeft == 0 && !hasWon(PLAYER_O) && !hasWon(PLAYER_X);
+    }
+
+
+}
---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,98 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+    private TicTacToe game;
+
+    @Before
+    public void setUp() {
+        game = new TicTacToe();
+    }
+
+    @Test
+    public void testInTheBeginningAllMovesAreLegal() {
+        for (int i = 0; i < 9; i++) {
+            Assert.assertTrue(game.isMoveLegal(i));
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalPositionForGetState() {
+        game.getState(100);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalPositionForPlay() {
+        game.playMove(TicTacToe.PLAYER_X, 100);
+    }
+
+    @Test
+    public void testRemembersMoves() {
+        game.playMove(TicTacToe.PLAYER_X, 3);
+        game.playMove(TicTacToe.PLAYER_O, 4);
+        Assert.assertEquals(game.getState(3), TicTacToe.PLAYER_X);
+        Assert.assertEquals(game.getState(4), TicTacToe.PLAYER_O);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testXCannotPlayOnO() {
+        game.playMove(TicTacToe.PLAYER_O, 0);
+        game.playMove(TicTacToe.PLAYER_X, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testXCannotPlayAfterX() {
+        game.playMove(TicTacToe.PLAYER_X, 0);
+        game.playMove(TicTacToe.PLAYER_X, 1);
+    }
+
+    @Test
+    public void testWinByVerticalLine() {
+        game.playMove(TicTacToe.PLAYER_X, 0);
+        game.playMove(TicTacToe.PLAYER_O, 4);
+        game.playMove(TicTacToe.PLAYER_X, 3);
+        game.playMove(TicTacToe.PLAYER_O, 8);
+        game.playMove(TicTacToe.PLAYER_X, 6);
+        Assert.assertTrue(game.hasWon(TicTacToe.PLAYER_X));
+    }
+
+    @Test
+    public void testWinByHorizontalLine() {
+        game.playMove(TicTacToe.PLAYER_X, 0);
+        game.playMove(TicTacToe.PLAYER_O, 3);
+        game.playMove(TicTacToe.PLAYER_X, 2);
+        game.playMove(TicTacToe.PLAYER_O, 4);
+        game.playMove(TicTacToe.PLAYER_X, 6);
+        game.playMove(TicTacToe.PLAYER_O, 5);
+        Assert.assertTrue(game.hasWon(TicTacToe.PLAYER_O));
+    }
+
+    @Test
+    public void testWinByDiagonalLine() {
+        game.playMove(TicTacToe.PLAYER_X, 0);
+        game.playMove(TicTacToe.PLAYER_O, 3);
+        game.playMove(TicTacToe.PLAYER_X, 4);
+        game.playMove(TicTacToe.PLAYER_O, 6);
+        game.playMove(TicTacToe.PLAYER_X, 8);
+        Assert.assertTrue(game.hasWon(TicTacToe.PLAYER_X));
+    }
+
+    @Test
+    public void testDraw() {
+        game.playMove(TicTacToe.PLAYER_X, 0);
+        game.playMove(TicTacToe.PLAYER_O, 1);
+        game.playMove(TicTacToe.PLAYER_X, 2);
+        game.playMove(TicTacToe.PLAYER_O, 3);
+        game.playMove(TicTacToe.PLAYER_X, 5);
+        game.playMove(TicTacToe.PLAYER_O, 4);
+        game.playMove(TicTacToe.PLAYER_X, 6);
+        game.playMove(TicTacToe.PLAYER_O, 8);
+        game.playMove(TicTacToe.PLAYER_X, 7);
+        Assert.assertFalse(game.hasWon(TicTacToe.PLAYER_X));
+        Assert.assertFalse(game.hasWon(TicTacToe.PLAYER_O));
+        Assert.assertTrue(game.isDraw());
+    }
+
+}
